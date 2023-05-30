54232ff10ff1e7f94663be304b2e8267adb7eea8
Theodore (Machine User) <CMI_CPAC_Support@childmind.org>
2023-05-30 11:35:49 -0400

    setup.py

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,9 +1,5 @@
 from .info import __version__
 version = __version__
 
-#from .overlay_nifti import *
-#__all__ = ['overlay_nifti']
-from .overlay_nifti import overlay_nifti
-
-if __name__ == '__main__':
-    overlay_nifti()
+from .overlay_nifti import *
+__all__ = ['overlay_nifti']
